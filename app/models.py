638166e83ec9de7a8fe9641722edf53b80eb4d75
Fernando <fernando.df05@gmail.com>
2021-05-13 18:38:53 -0300

    DB

--- app/models.py
+++ app/models.py
@@ -6,4 +6,4 @@ class Organizadora(models.Model):
     cnpj = models.IntegerField(primary_key= True)
     nome_real = models.CharField(max_length=45)
     nome_fantasia = models.CharField(max_length=45)
-
+    
